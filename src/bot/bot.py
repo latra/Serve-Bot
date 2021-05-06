fb2317fbe8147b334c9d932eb6ab1dd1cb30479d
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-05-06 16:17:17 +0200

    control de game

--- src/bot/bot.py
+++ src/bot/bot.py
@@ -20,7 +20,7 @@ class DiscordBot:
 
         intents = discord.Intents.all()
 
-        self.client = discord_commands.Bot(command_prefix='!')
+        self.client = discord_commands.Bot(command_prefix=os.getenv('DISCORD_PREFIX'))
         self.token = os.getenv('DISCORD_TOKEN')
         self.index = 0
         self.client.remove_command('help')
