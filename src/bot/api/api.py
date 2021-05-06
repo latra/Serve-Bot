f0be74907de7d5a3ca73377a01b2a7a2163c030d
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-05-06 09:50:21 +0200

    api post changed

--- src/bot/api/api.py
+++ src/bot/api/api.py
@@ -2,7 +2,7 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
 from flask import request
-from multiprocessing import Process
+import asyncio
 
         
 class BotApi: 
@@ -13,7 +13,7 @@ class BotApi:
         @self.app.route('/game', methods = ['POST'])
         def gameServer():
             body_json = request.json
-            Process(target=self.game_response, args=(body_json)).start()
+            asyncio.run(self.game_response(body_json))
             print(request)
 
             return 'OK'
