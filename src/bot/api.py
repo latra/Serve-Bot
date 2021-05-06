158dffd5c744e72654fa42bdcad413b0d575b683
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-05-06 11:21:44 +0200

    api post changed

--- src/bot/api.py
+++ src/bot/api.py
@@ -35,8 +35,8 @@ class BotApi:
             await channel.send('test')
             exit()
         print("Aqui llega!!!!!!!!")
-        client.run(self.token)
-
+        loop = asyncio.get_event_loop()
+        loop.run_until_complete(client.start(self.token))
     def start(self):
         
         print("STARTING!")
