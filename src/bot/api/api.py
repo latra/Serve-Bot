4a17bb3ae74553cb2e25ba37572419967cd35dab
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-05-06 10:02:57 +0200

    api post changed

--- src/bot/api/api.py
+++ src/bot/api/api.py
@@ -21,13 +21,11 @@ class BotApi:
     async def game_response(self, body_json):
         await self.treatment(body_json['server_uid'], body_json['channel_uid'], body_json['game'], body_json['status'], body_json['ip'], body_json['port'])
     async def treatment(self, server_uid, channel_uid, game, status, ip, port):
-        @self.client.event
-        async def on_ready():
-            print("channel: " + channel_uid)
-            channel = self.client.get_channel(channel_uid)
-            await channel.send('test')
-            exit()
+
         await self.client.start(self.token)
+        print("channel: " + channel_uid)
+        channel = self.client.get_channel(channel_uid)
+        await channel.send('test')
     def start(self):
         
         print("STARTING!")
