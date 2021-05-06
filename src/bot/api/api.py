4943f4de1f417ba40672ff4ef18ec3e2b7c675bd
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-05-06 10:22:59 +0200

    api post changed

--- src/bot/api/api.py
+++ src/bot/api/api.py
@@ -3,11 +3,11 @@ from flask_restful import Resource, Api
 from json import dumps
 from flask import request
 import asyncio
+from discord.ext import commands as discord_commands
 
         
 class BotApi: 
-    def __init__(self, client, token):
-        self.client = client
+    def __init__(self, token):
         self.app = Flask(__name__)
         self.api = Api(self.app)
         self.token = token
@@ -21,14 +21,16 @@ class BotApi:
     async def game_response(self, body_json):
         await self.treatment(body_json['server_uid'], body_json['channel_uid'], body_json['game'], body_json['status'], body_json['ip'], body_json['port'])
     async def treatment(self, server_uid, channel_uid, game, status, ip, port):
+
+        client = discord_commands.Bot()
         @self.client.event
         async def on_ready():
             print("channel: " + channel_uid)
-            channel = self.client.get_channel(channel_uid)
+            channel = client.get_channel(channel_uid)
             await channel.send('test')
             exit()
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(self.client.run(self.token))
+        loop.run_until_complete(client.run(self.token))
 
     def start(self):
         
