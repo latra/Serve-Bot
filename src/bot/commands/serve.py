c896bab6a3a76ab90a6bb61f680df7ebc0d51d06
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-05-06 22:35:22 +0200

    control de game

--- src/bot/commands/serve.py
+++ src/bot/commands/serve.py
@@ -11,10 +11,12 @@ async def terraria(ctx):
     await ctx.send(lang_string[TERRARIA_ASKED])
     game = "terraria"
     request_body = {'server_uid':ctx.guild.id , 'game':game, 'callback_url': bot_ip}
-    x = requests.post(url, data = request_body, headers= {'token':os.getenv("API_TOKEN")})
-    if x.status_code == 200:
-        await ctx.send(lang_string[TERRARIA_TRUE])
-    elif x.status_code == 403:
-        await ctx.send(lang_string[TERRARIA_ERROR_FORBBIDEN])
-    elif x.status_code == 440:
-        await ctx.send(lang_string[TERRARIA_ERROR_RUNNING])
+    await ctx.send("DUMMY MESSAGE. BACKEND NOT RUNNING")
+
+    # x = requests.post(url, data = request_body, headers= {'token':os.getenv("API_TOKEN")})
+    # if x.status_code == 200:
+    #     await ctx.send(lang_string[TERRARIA_TRUE])
+    # elif x.status_code == 403:
+    #     await ctx.send(lang_string[TERRARIA_ERROR_FORBBIDEN])
+    # elif x.status_code == 440:
+    #     await ctx.send(lang_string[TERRARIA_ERROR_RUNNING])
