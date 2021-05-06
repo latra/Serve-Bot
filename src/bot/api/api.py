8ea9ae90333ed65480cbc4900ef648717924031d
Paula (Latra) Gallucci <personadelmonton@gmail.com>
2021-05-06 09:46:17 +0200

    api post changed

--- src/bot/api/api.py
+++ src/bot/api/api.py
@@ -2,6 +2,8 @@ from flask import Flask, request
 from flask_restful import Resource, Api
 from json import dumps
 from flask import request
+from multiprocessing import Process
+
         
 class BotApi: 
     def __init__(self, client):
@@ -9,14 +11,15 @@ class BotApi:
         self.app = Flask(__name__)
         self.api = Api(self.app)
         @self.app.route('/game', methods = ['POST'])
-        async def gameServer():
+        def gameServer():
             body_json = request.json
+            Process(target=self.game_response, args=(body_json)).start()
             print(request)
-            print("NO COMPRENDO AMIGO")
-           # channel = self.client.get_channel(body_json['channel_uid'])
-            #await channel.send('test')
-            return 'OK'
 
+            return 'OK'
+    async def game_response(self, body_json):
+            channel = self.client.get_channel(body_json['channel_uid'])
+            await channel.send('test')
     def start(self):
         print("STARTING!")
         self.app.run(host="0.0.0.0", port='4030')
