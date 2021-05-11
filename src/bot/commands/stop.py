0a2a6b22a881826340d5219a00a9fce047a1efd9
Latra <personadelmonton@gmail.com>
2021-05-11 18:14:27 +0200

    Command stop + error message when server not running

--- src/bot/commands/stop.py
+++ src/bot/commands/stop.py
@@ -2,27 +2,32 @@ from utils.constants import *
 import os
 import requests
 import json
+import logging
+from requests.exceptions import HTTPError
+from requests.exceptions import RequestException
+class ServeCommand:
+    def __init__(self):
+        self.lang_string = json.load(open(os.path.join(os.path.dirname(os.path.realpath('__file__')), 'src/bot/strings/ES-es.json')))
+        self.url = os.getenv("SERVER_URL")
+        self.game = ""
+        self.bot_ip = os.getenv("BOT_API")
+        self.api_token = os.getenv("API_TOKEN")
+    async def terraria(self, ctx):
+        await ctx.send(self.lang_string[TERRARIA_ASKED])
+        logging.info(f"Enviando petición a {self.url}")
+        self.game = "terraria"
+        request_body = {
+            'server_uid':ctx.guild.id,
+            'game': self.game,
+            'action':'stop'}
 
-lang_string = json.load(open(os.path.join(os.path.dirname(os.path.realpath('__file__')), 'src/bot/strings/ES-es.json')))
-url = os.getenv("SERVER_URL")
-game = ""
-bot_ip = os.getenv("BOT_API")
-async def terraria(ctx):
-    await ctx.send(lang_string[TERRARIA_ASKED])
-    game = "terraria"
-    request_body = {
-        'server_uid':ctx.guild.id,
-        'game':game,
-        'action':'stop'
-    }
-
-    try:
-        x = requests.post(url, data = request_body, headers= {'token':os.getenv("API_TOKEN")})
-        if x.status_code == 200:
-            await ctx.send(lang_string[TERRARIA_STOP])
-        elif x.status_code == 403:
-            await ctx.send(lang_string[TERRARIA_ERROR_FORBBIDEN])
-        elif x.status_code == 440:
-            await ctx.send(lang_string[TERRARIA_ERROR_STOPPED])
-    except TimeoutError as ex:
-        await ctx.send(lang_string[SERVER_COMUNICATION_ERROR])
+        try:
+            x = requests.post(self.url, data = request_body, headers= {'token':self.api_token})
+            if x.status_code == 200:
+                await ctx.send(self.lang_string[TERRARIA_STOP])
+            elif x.status_code == 403:
+                await ctx.send(self.lang_string[TERRARIA_ERROR_FORBBIDEN])
+            elif x.status_code == 440:
+                await ctx.send(self.lang_string[TERRARIA_ERROR_STOPPED])
+        except (HTTPError, RequestException):
+            await ctx.send(self.lang_string[SERVER_COMUNICATION_ERROR])
